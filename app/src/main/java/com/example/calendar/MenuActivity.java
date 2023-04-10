ef793c68c7a9ce1c779893ad2c7821a7c1f333dd
AgeDim <proga778@gmail.com>
2023-04-10 21:08:23 +0300

    Start to do bottom NavBar

--- app/src/main/java/com/example/calendar/MenuActivity.java
+++ app/src/main/java/com/example/calendar/MenuActivity.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+
 public class MenuActivity extends AppCompatActivity {
+
+    BottomNavigationBar bottomNavigationBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        bottomNavigationBar = findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationBar
+                .setActiveColor(R.color.primary)
+                .setInActiveColor("#ffccff")
+                .setBarBackgroundColor("#ffccff");
+        bottomNavigationBar
+                .addItem(new BottomNavigationItem(R.drawable.calc, ""))
+                .addItem(new BottomNavigationItem(R.drawable.user, ""))
+                .addItem(new BottomNavigationItem(R.drawable.history, ""))
+                .addItem(new BottomNavigationItem(R.drawable.working_time, ""))
+                .setFirstSelectedPosition(0)
+                .initialise();
     }
 }
