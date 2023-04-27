5fd5bacfd14e4e12ae6069caff636cff39754c03
AgeDim <proga778@gmail.com>
2023-04-27 15:14:01 +0300

    Added calendar with selection of Day

--- app/src/main/java/com/example/calendar/UserFragment.java
+++ app/src/main/java/com/example/calendar/UserFragment.java
@@ -1,47 +1,70 @@
 package com.example.calendar;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import com.applandeo.materialcalendarview.EventDay;
-import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
-import com.applandeo.materialcalendarview.CalendarView;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.rengwuxian.materialedittext.MaterialEditText;
+import com.skyhope.eventcalenderlibrary.CalenderEvent;
+import com.skyhope.eventcalenderlibrary.listener.CalenderDayClickListener;
+import com.skyhope.eventcalenderlibrary.model.DayContainerModel;
+import com.skyhope.eventcalenderlibrary.model.Event;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
 
 public class UserFragment extends Fragment {
 
-    CalendarView calendarView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View tempView = inflater.inflate(R.layout.fragment_user, container, false);
-        calendarView = tempView.findViewById(R.id.calendarView);
-        calendarView.setOnDayClickListener(new OnDayClickListener() {
-            @Override
-            public void onDayClick(@NonNull EventDay eventDay) {
-                Calendar clickedDayCalendar = eventDay.getCalendar();
-//                if (eventDay.getLabelColor$library_release() != R.color.red) {
-//                    eventDay.setLabelColor$library_release(R.color.red);
-//                } else {
-//                    eventDay.setLabelColor$library_release(R.color.white);
-//                }
-//                List<Calendar> calendars = new ArrayList<>();
-//                calendarView.setHighlightedDays(calendars);
-//                clickedDayCalendar.set();
-            }
-        });
+        CalenderEvent calenderEvent = tempView.findViewById(R.id.calender_event);
 
+        calenderEvent.initCalderItemClickCallback(new CalenderDayClickListener() {
+            @Override
+            public void onGetDay(DayContainerModel dayContainerModel) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(tempView.getContext());
+                dialog.setTitle("Day");
+                LayoutInflater inflater = LayoutInflater.from(tempView.getContext());
+                View eventWindow = inflater.inflate(R.layout.setevent_layout, null);
+                dialog.setView(eventWindow);
+                MaterialEditText workingTime = eventWindow.findViewById(R.id.workingTm);
+                MaterialEditText hourSalary = eventWindow.findViewById(R.id.salary);
+                MaterialEditText additionalHour = eventWindow.findViewById(R.id.additHour);
+                dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                dialog.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Event event = new Event(dayContainerModel.getTimeInMillisecond(), "Test", Color.RED);
+                        calenderEvent.addEvent(event);
+                    }
+                });
+                dialog.show();
+            }
+
+        });
         return tempView;
     }
+
 }
